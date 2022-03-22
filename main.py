0489ff3f27f3959278cf47a4e4393a8826ec5ca9
kartikay srivastava <34645529+KartikaySrivadtava@users.noreply.github.com>
2022-03-22 11:35:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ FACTOR = 16
 
 
 def load_data():
-    data = pd.read_csv(os.path.dirname(__file__) + '/data/rwhar_data.csv', sep=',')
-    return data
+    df = pd.read_csv(os.path.dirname(__file__) + '/data/rwhar_data.csv', sep=',')
+    return df
 
 
 def downsampling(df, n):
@@ -70,65 +70,51 @@ def resampling(data, n):
 
 def evaluation():
     print("Inside evaluation")
+
+    # Load csv file as dataframe containing values of all activities using predict function of the original program
+
     eval_df = pd.read_csv(os.path.dirname(__file__) + '/predictions/f1_final.csv', sep=',',
                           index_col=None, header=None)
-    # print(eval_df)
-    df_climb_down = eval_df.loc[eval_df[1] == 'climbingdown']
+
+    # Create dataframes containing values of a particular activity at all frequencies
+
     df_climb_up = eval_df.loc[eval_df[1] == 'climbingup']
+    df_climb_down = eval_df.loc[eval_df[1] == 'climbingdown']
     df_jumping = eval_df.loc[eval_df[1] == 'jumping']
     df_lying = eval_df.loc[eval_df[1] == 'lying']
     df_running = eval_df.loc[eval_df[1] == 'running']
     df_sitting = eval_df.loc[eval_df[1] == 'sitting']
     df_standing = eval_df.loc[eval_df[1] == 'standing']
     df_waling = eval_df.loc[eval_df[1] == 'walking']
-    list_act = ['50Hz', '25Hz', '12.5', '6.25Hz', '3.125Hz']
-    list_climb_down = df_climb_down[0]
-    list_climb_up = df_climb_up[0]
-    list_jumping = df_jumping[0]
-    list_lying = df_lying[0]
-    list_running = df_running[0]
-    list_sitting = df_sitting[0]
-    list_standing = df_standing[0]
-    list_walking = df_waling[0]
-    # print(list_climb_up)
-    dict_climb_up = dict(zip(list_act, list_climb_up))
-    dict_climb_down = dict(zip(list_act, list_climb_down))
-    dict_jump = dict(zip(list_act, list_jumping))
-    dict_lying = dict(zip(list_act, list_lying))
-    dict_run = dict(zip(list_act, list_running))
-    dict_sit = dict(zip(list_act, list_sitting))
-    dict_stand = dict(zip(list_act, list_standing))
-    dict_walk = dict(zip(list_act, list_walking))
 
-    dict_climb_up = dict(reversed(list(dict_climb_up.items())))
-    dict_climb_down = dict(reversed(list(dict_climb_down.items())))
-    dict_jump = dict(reversed(list(dict_jump.items())))
-    dict_lying = dict(reversed(list(dict_lying.items())))
-    dict_run = dict(reversed(list(dict_run.items())))
-    dict_sit = dict(reversed(list(dict_sit.items())))
-    dict_stand = dict(reversed(list(dict_stand.items())))
-    dict_walk = dict(reversed(list(dict_walk.items())))
+    # Create list of values for each activity starting from 50 Hz to 3.125 Hz
 
-    print("Dictionary for climb up", dict_climb_up)
-    print("Dictionary for climb down", dict_climb_down)
-    print("Dictionary for jumping", dict_jump)
-    print("Dictionary for lying", dict_lying)
-    print("Dictionary for running", dict_run)
-    print("Dictionary for sitting", dict_sit)
-    print("Dictionary for standing", dict_stand)
-    print("Dictionary for walking", dict_walk)
+    list_climb_up = (np.array(df_climb_up.iloc[:, 0])).astype(float)
+    list_climb_down = (np.array(df_climb_down.iloc[:, 0])).astype(float)
+    list_jumping = (np.array(df_jumping.iloc[:, 0])).astype(float)
+    list_lying = (np.array(df_lying.iloc[:, 0])).astype(float)
+    list_running = (np.array(df_running.iloc[:, 0])).astype(float)
+    list_sitting = (np.array(df_sitting.iloc[:, 0])).astype(float)
+    list_standing = (np.array(df_standing.iloc[:, 0])).astype(float)
+    list_walking = (np.array(df_waling.iloc[:, 0])).astype(float)
+
+    # Generic list of frequencies at which these F1 scores have been recorded
+
+    list_freq = ['50Hz', '25Hz', '12.5', '6.25Hz', '3.125Hz']
+
+    # Plotting for all activities F1 score begins from here
 
     # Climbing up
-    x = np.array(["3.125Hz", "6.25Hz", "12.5Hz", "25Hz", "50Hz"])
-    y = np.array([0.16120577, 1.46228751e-01, 0.12200513, 0.16865588, 0.17731791])
+    x = list_freq
+    y = list_climb_up
     ax1 = plt.subplot(2, 4, 1)
     ax1.set_ylim([0, 1])
     ax1.bar(x, y)
     ax1.set_title('F1 climbing up')
 
     # Climbing down
-    x = np.array(["3.125Hz", "6.25Hz", "12.5Hz", "25Hz", "50Hz"])
-    y = np.array([0.00894569, 6.42054575e-04, 0.00737898, 0.55602312, 0.50007438])
+    x = list_freq
+    y = list_climb_down
     ax2 = plt.subplot(2, 4, 2)
     ax2.bar(x, y)
     ax2.set_yticklabels([])
@@ -136,8 +122,8 @@ def evaluation():
     ax2.set_title('F1 climbing down')
 
     # Jumping
-    x = np.array(["3.125Hz", "6.25Hz", "12.5Hz", "25Hz", "50Hz"])
-    y = np.array([0.0, 2.80112045e-03, 0.07974684, 0.79935012, 0.93278539])
+    x = list_freq
+    y = list_jumping
     ax3 = plt.subplot(2, 4, 3)
     ax3.bar(x, y)
     ax3.set_yticklabels([])
@@ -145,9 +131,8 @@ def evaluation():
     ax3.set_title('F1 jumping')
 
     # Lying
-    x = np.array(["3.125Hz", "6.25Hz", "12.5Hz", "25Hz", "50Hz"])
-    y = np.array([0.69628775, 6.86327992e-01, 0.71235705, 0.72834384, 0.71312762])
-
+    x = list_freq
+    y = list_lying
     ax4 = plt.subplot(2, 4, 4)
     ax4.bar(x, y)
     ax4.set_yticklabels([])
@@ -155,9 +140,8 @@ def evaluation():
     ax4.set_title('F1 lying')
 
     # Running
-    x = np.array(["3.125Hz", "6.25Hz", "12.5Hz", "25Hz", "50Hz"])
-    y = np.array([0.74904858, 4.12183055e-01, 0.68574628, 0.87960257, 0.90076908])
-
+    x = list_freq
+    y = list_running
     ax5 = plt.subplot(2, 4, 5)
     ax5.bar(x, y)
     ax5.set_yticklabels([])
@@ -165,9 +149,8 @@ def evaluation():
     ax5.set_title('F1 running')
 
     # Sitting
-    x = np.array(["3.125Hz", "6.25Hz", "12.5Hz", "25Hz", "50Hz"])
-    y = np.array([0.57946871, 5.39011634e-01, 0.54231007, 0.53954559, 0.5122115])
-
+    x = list_freq
+    y = list_sitting
     ax6 = plt.subplot(2, 4, 6)
     ax6.bar(x, y)
     ax6.set_yticklabels([])
@@ -175,9 +158,8 @@ def evaluation():
     ax6.set_title('F1 Sitting')
 
     # Standing
-    x = np.array(["3.125Hz", "6.25Hz", "12.5Hz", "25Hz", "50Hz"])
-    y = np.array([0.45651606, 4.15405139e-01, 0.66609605, 0.77698193, 0.76365756])
-
+    x = list_freq
+    y = list_standing
     ax7 = plt.subplot(2, 4, 7)
     ax7.bar(x, y)
     ax7.set_yticklabels([])
@@ -185,9 +167,8 @@ def evaluation():
     ax7.set_title('F1 Standing')
 
     # Walking
-    x = np.array(["3.125Hz", "6.25Hz", "12.5Hz", "25Hz", "50Hz"])
-    y = np.array([0.18834297, 1.98414867e-01, 0.49548176, 0.66411244, 0.66245299])
-
+    x = list_freq
+    y = list_walking
     ax8 = plt.subplot(2, 4, 8)
     ax8.bar(x, y)
     ax8.set_yticklabels([])
@@ -198,8 +179,12 @@ def evaluation():
     plt.subplots_adjust(wspace=0)
     plt.show()
 
-    # Overall best results
-    acc = np.array([0.17731791, 0.55602312, 0.93278539, 0.72834384, 0.90076908, 0.57946871, 0.77698193, 0.66411244])
+    # Plotting for overall best results
+
+    acc = np.array(
+        [np.max(list_climb_up), np.max(list_climb_down), np.max(list_jumping), np.max(list_lying), np.max(list_running),
+         np.max(list_sitting), np.max(list_standing), np.max(list_walking)])
+    print("acc is ", acc)
     act = np.array(["Climbing-up 50Hz", "Climbing-down 25Hz ", "Jumping 50Hz ", "Lying 25Hz ", "Running 50Hz ",
                     "Sitting 3.125Hz ", "Standing 25Hz ", "Walking 25Hz "])
     c = ['red', 'green', 'red', 'green', 'red', 'blue', 'green', 'green']
@@ -209,6 +194,8 @@ def evaluation():
     plt.ylabel("Accuracy")
     plt.show()
 
+    # Plot for overall savings that can be deducted by using best F1 score for each activity from the previous plots
+
     act = np.array(
         ["Climbing-up", "Climbing-down", "Jumping", "Lying", "Running", "Sitting", "Standing", "Walking", "Total"])
     savings = np.array([0.0, 50.0, 0.0, 50.0, 0.0, 62.5, 50.0, 50.0, 262.5])
@@ -216,7 +203,7 @@ def evaluation():
     plt.bar(act, savings, color=c)
     plt.title("Total saving by picking best accuracy")
     plt.xlabel("Activity")
-    plt.ylabel("Savings")
+    plt.ylabel("Savings as percentage")
     plt.show()
 
 
